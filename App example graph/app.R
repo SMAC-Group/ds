7ce89d46f27d2ed0a85e5ad831d6739c5fc8cdd8
Stéphane Guerrier <stephaneguerrier@Stephanes-MBP.home>
2017-08-04 18:47:50 +0200

    Minor changes with shiny app on graphs

--- App example graph/app.R
+++ App example graph/app.R
@@ -14,11 +14,14 @@ ui <- fluidPage(
 
    # Application title
    titlePanel(" "),
+  
 
    # Sidebar with a slider input for number of bins
    sidebarLayout(
-      sidebarPanel(
 
+      sidebarPanel(
+        h4("Maybe add a title?"),
+        br(),
         selectInput("type", label = "type = ",
                     choices = list("b" = "b", "l" = "l", "p" = "p",
                                    "s" = "s", "h" = "h"),
@@ -92,9 +95,11 @@ ui <- fluidPage(
 
       ),
 
-      # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+        tabsetPanel(type = "tabs", 
+                    tabPanel("Plot", plotOutput("distPlot")), 
+                    tabPanel("Show code", verbatimTextOutput("code"))
+        )
       )
   , position = "right")
 )
@@ -126,7 +131,114 @@ server <- function(input, output) {
                lty = input$lty, col = input$col, pt.cex = input$cex,
                bty = input$bty, pch = input$pch, cex = input$leg_cex)
       }
+        
+      
+      
 
+   })
+   
+   output$code <- renderPrint({
+     cat("# Generate data\n")
+     cat("xx = seq(from = 0, to = 4*pi, length.out = 60)\n")
+     cat("yy = sin(xx)\n")
+     
+     cat("\n")
+     cat("# Main plot\n")
+     cat("plot(xx, yy, main = '")
+     cat(input$main)
+     cat("', xlab = '")
+     cat(input$xlab)
+     cat("', ylab = '")   
+     cat(input$ylab)
+     cat("', type = '")   
+     cat(input$type)
+     cat("'")
+     
+     if (input$type == "b" || input$type == "b"){
+       cat(", pch = ")   
+       cat(input$pch)
+     }
+     
+     
+     if (input$cex != 1 & (input$type == "b" || input$type == "b")){
+       cat(", cex = ")   
+       cat(input$pch)
+     }
+     
+     if (input$lwd != 1 & input$type != "p"){
+       cat(", lwd = ")   
+       cat(input$lwd)
+     }
+     
+     if (input$lty != 1 & input$type != "p"){
+       cat(", lty = ")   
+       cat(input$lty)
+     }
+     
+     
+     if (input$col != 1){
+       cat(",  col = ")   
+       cat(input$col)
+     }
+   
+     if (input$col.main != 1){
+       cat(", col.main = ")
+       cat(input$col.main)
+     }
+     
+     if (input$col.lab != 1){
+       cat(", col.lab = ")
+       cat(input$col.lab)
+     }
+     
+     if (input$col.axis != 1){
+       cat(", col.axis = ")
+       cat(input$col.axis)
+     }
+     
+     if (input$cex.main != 1){
+       cat(", cex.main = ")
+       cat(input$cex.main)
+     }
+     
+     if (input$cex.lab != 1){
+       cat(", cex.lab = ")
+       cat(input$cex.lab)
+     }
+     
+     if (input$cex.axis != 1){
+       cat(", cex.axis = ")
+       cat(input$cex.axis)
+     }
+     
+     cat(")\n\n")
+     
+     
+     if (input$grid){
+       cat("# Add legend\n")
+       cat("grid()\n\n")
+     }
+     
+     if (input$legend){
+       cat("# Add legend\n")
+       cat("legend('")
+       cat(input$legend_position)
+       cat("', 'sin(x)', pt.lwd = ")
+       cat(input$lwd)
+       cat(", lty = ")
+       cat(input$lty)
+       cat(", col = ")
+       cat(input$col)
+       cat(", pt.cex = ")
+       cat(input$cex)
+       cat(", bty = '")
+       cat(input$bty)
+       cat("', pch = ")
+       cat(input$pch)
+       cat(", cex = ")
+       cat(input$leg_cex)
+       cat(")")
+     }
 
    })
 }
